ffc81ef863d52060981390bfff36f07396604db9
Zilton Cordeiro Junior <zilton@dionisio.speed.dcc.ufmg.br>
2018-07-11 15:10:45 -0300

    Estimator test working.

--- src/test/scala/br/ufmg/dcc/speed/lemonade/nlp/NameEntityRecognitionEstimatorTest.scala
+++ src/test/scala/br/ufmg/dcc/speed/lemonade/nlp/NameEntityRecognitionEstimatorTest.scala
@@ -3,8 +3,6 @@ package br.ufmg.dcc.speed.lemonade.nlp
 import com.github.mrpowers.spark.fast.tests.DataFrameComparer
 import org.scalatest.{FunSpec, Tag}
 
-import edu.stanford.nlp.ie.crf.CRFCliqueTree
-
 class NameEntityRecognitionEstimatorSpec extends FunSpec
 with SparkSessionTestWrapper  with DataFrameComparer {
 
@@ -14,7 +12,7 @@ with SparkSessionTestWrapper  with DataFrameComparer {
     var sourceDF = Seq(
       //("President Obama went to Rome to meed Pope."),
       //("Catholic Church agrees to provide documents regarding Galileu Galilei .")
-      ("O Lula sempre foi um safado e quase saiu da cadeia em Curitiba"),
+      ("Lula sempre foi um safado e quase saiu da cadeia em Curitiba"),
       ("O Tribunial Regional Federal quase soltou um ex-presidente do Brasil.")
     ).toDF("document")
     val estimator = new NamedEntityRecognitionEstimator()
@@ -26,12 +24,7 @@ with SparkSessionTestWrapper  with DataFrameComparer {
     val model = estimator.fit(sourceDF)
     var resultDF = model.transform(sourceDF)
 
-    estimator.g
-    CRFCliqueTree.getCalibratedCliqueTree()
-
-    println(resultDF.summary())
     resultDF.show(truncate = false)
-
 
     //model.save("file:///tmp/lixo.spark")
     assertSmallDataFrameEquality(sourceDF, sourceDF)
